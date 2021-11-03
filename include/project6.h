ece0ff2b0c48919814fe72dd921a30db27ec6a50
Alex Reyes Aranda <alexreyesaranda@students.berkeley.net>
2021-11-03 15:02:58 -0700

    mmkk

---
+++ include/project6.h
@@ -0,0 +1,6 @@
+#ifndef PROJECT6_H
+#define PROJECT6_H
+
+#include "main.h"
+void project6(int inches);
+#endif
---
+++ src/project6.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include "project6.h"
+#include "chassis.h"
+
+void project6(int inches) {
+
+}
