8c6dd487fb7b3b7faf3d753b1ba8beb228d2a5a2
Alex Reyes Aranda <alexreyesaranda@students.berkeley.net>
2021-11-08 15:28:27 -0800

    tried to fix holdshoulder

--- .vscode/cquery_cached_index/c@@users@period 6@myactualfirstproject/src@opcontrol.c
+++ .vscode/cquery_cached_index/c@@users@period 6@myactualfirstproject/src@opcontrol.c
@@ -38,9 +38,41 @@ SHOULDER = 4
  *
  * This task should never exit; it should end with some kind of infinite loop, even if empty.
  */
+ int boolean = 1;
 
  void holdShoulderAt(int td) { //target degree
+   Encoder shoulderEncoder;
+   shoulderEncoder = encoderInit(QUAD_TOP_PORT, QUAD_BOTTOM_PORT, false);
+
    int kp = 1;
+   int maxp = 127;
+   int error;
+   int output;
+
+   while(boolean == 1) {
+     error = td - encoderGet(shoulderEncoder);
+     output = kp * error;
+     if(abs(error) < maxp) {
+       liftSet(output);
+     }else {
+       liftSet(output / abs(output) * maxp);
+     }
+   }
+   }
+   /*while(true) {
+
+     int kp = 1;
+     int maxd = 127;
+       int error = td - encoderGet(shoulderEncoder);
+       int output = kp * error;
+       if(abs(error)  < maxd) {
+         liftSet(output);
+       } else {
+         liftSet(output/ abs(output)*maxd);
+       }
+     }
+   }*/
+   /**int kp = 1;
    int maxd = 127;
    Encoder shoulderEncoder;
    shoulderEncoder = encoderInit(QUAD_TOP_PORT, QUAD_BOTTOM_PORT, true);
@@ -51,24 +83,25 @@ SHOULDER = 4
      } else {
        liftSet(output/ abs(output)*maxd);
      }
-   }
-
+   }**/
+//-17
 void operatorControl() {
   int power, turn;
   Encoder sEncoder;
-  sEncoder = encoderInit(QUAD_TOP_PORT, QUAD_BOTTOM_PORT, true);
+  sEncoder = encoderInit(QUAD_TOP_PORT, QUAD_BOTTOM_PORT, false);
 //joystickGetAnalog(unsigned char joystick, unsigned char axis)
   //printf("before the while loop in opcontrol \n");
   while(1) {
-    printf("Encoder value is at: %d \n", encoderGet(sEncoder));
-  //  printf("hello main loop print \n");
+    //printf("Hello World! \n");
+    //printf("Encoder value is at: %d \n", encoderGet(sEncoder));
+  // printf("hello main loop print \n");
     //holdShoulderAt(50);
   		power = joystickGetAnalog(1,1); //vertical axis on right joystick
   		turn = joystickGetAnalog(1,2); //horizontal axis on right
       chassisSet(power - turn, power + turn);
       //printf("the pot value %d \n", pot);
       //chassisSet(power - turn, power + turn);
-  		//motorSet(1, power - turn);// right wheels
+  		//motorSet(1 {}, power - turn);// right wheels
   		//motorSet(10,power + turn); // left wheels
       //clawSet(joystickGetAnalog(1, 4));
 
@@ -89,13 +122,25 @@ void operatorControl() {
       }
       if(joystickGetDigital(1,8, JOY_RIGHT )) {
         printf("calling holdShoulderAt \n");
-        holdShoulderAt(100);
+        holdShoulderAt(50);
         //project6();
       }
+      while(joystickGetDigital(1, 8, JOY_UP)) {
+        printf("Encoder value is at: %d \n", encoderGet(sEncoder));
+      }
+      if(joystickGetDigital(1,8,JOY_LEFT)) {
+        printf("test \n");
+      }
+      if (joystickGetDigital(1,8, JOY_DOWN)) {
+        boolean = 0;
+      }
       if (joystickGetDigital(1, 8,JOY_UP)) {
+        //printf("Encoder value is at: %d \n", encoderGet(sEncoder));
         //printf("Calling homeshoulder its commented out rn tho \n");
       //commenting out for now  homeShoulder(50);
+
       }
+
 
   		delay(200);
 
